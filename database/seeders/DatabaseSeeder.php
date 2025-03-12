b3a1a7a2b62467fcbe875b2256b9e441a5f120c8
Sistemas-113 <matriculasonlineanagustin.edu.co>
2025-03-12 17:18:56 -0500

    Arreglando seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
