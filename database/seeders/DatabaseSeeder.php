c1f81bb8661931d130b69e0e9c71ea4bd89cfc81
Sistemas-113 <matriculasonlineanagustin.edu.co>
2025-03-12 17:16:20 -0500

    Integrando los Seeder al Model User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+       DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'danilo.doria@sanagustin.edu.co',
+            'password' => Hash::make('Sistemas113'),
         ]);
     }
 }
